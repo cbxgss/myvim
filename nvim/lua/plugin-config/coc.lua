853ea3569814b38a7669c5208132a9f99b648887
chengrong <chengrong@tju.edu.cn>
2023-03-01 16:59:37 +0800

    👌 （coc): 快捷键

--- nvim/lua/plugin-config/coc.lua
+++ nvim/lua/plugin-config/coc.lua
@@ -1,4 +1,18 @@
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "<F12>", "<Plug>(coc-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
 vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+-- 显示文档提示
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+vim.api.nvim_set_keymap("n", "<leader>k", '<CMD>lua _G.show_docs()<CR>', {silent = true})
--- nvim/lua/plugin-config/whichkey.lua
+++ nvim/lua/plugin-config/whichkey.lua
@@ -4,4 +4,5 @@ local wk = require("which-key")
 wk.register({
     ["<leader>f"] = { name = "文件搜索" },
     ["<leader>c"] = { name = "注释" },
+    ["<leader>k"] = { name = "文档提示" },
 })
